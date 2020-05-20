110320f353af4e40730dbae9a738b1be1d256487
Praphul Nangeelil <pnangeelil@byrider.com>
2020-05-21 00:49:00 +0530

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ContactListWithMVC_React.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ wwwroot/js/ContactList.js
@@ -0,0 +1,46 @@
+﻿const data = [
+    {
+        name: "Praphul",
+        email: "pnangeelil@suyati.com",
+    },
+    {
+        name: "Vishnu",
+        email: "vrajendran@suyati.com",
+    },
+    {
+        name: "Georgekutty",
+        email: "ggeorge@suyati.comm",
+    },
+];
+
+
+const ContactList = ({ contacts }) => {
+    return <table className="contacts-table">
+        <tbody>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+            </tr>
+            {contacts.map((contacts, i) => {
+                return <tr key={i}>
+                    <td>{contacts.name}</td>
+                    <td>{contacts.email}</td>
+                </tr>
+            })}
+        </tbody>
+    </table>;
+};
+
+const contactManagement1 = <section id="contact-list">
+    <h1>Contacts</h1>
+    <ContactList contacts={data}>
+
+    </ContactList>
+</section>;
+
+
+ReactDOM.render(
+    contactManagement1,
+    document.getElementById("react-container")
+);
+
